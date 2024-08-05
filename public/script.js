8a726ac3eda9f1b3ae0102fba86ce923d4daab5a
Conor Meade <cmeade6479@gmail.com>
2024-08-05 00:51:07 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const success = await loginUser(email, password);
     if (success) {
-      window.location.href = 'https://infinite-beyond-05850-58f77000e905.herokuapp.com/public/main_page.html';
+      window.location.href = 'https://infinite-beyond-05850-58f77000e905.herokuapp.com/main_page.html';
     } else {
       alert('Invalid email or password.');
     }
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const success = await registerUser(name, username, password, email);
     if (success) {
       alert('Registration successful!');
-      window.location.href = 'https://infinite-beyond-05850-58f77000e905.herokuapp.com/public/main_page.html'; // REPLACE LATER with a new URL
+      window.location.href = 'https://infinite-beyond-05850-58f77000e905.herokuapp.com/main_page.html'; // REPLACE LATER with a new URL
     } else {
       alert('Registration failed.');
     }
