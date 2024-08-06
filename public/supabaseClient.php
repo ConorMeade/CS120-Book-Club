f87852b6f09625437aaf78ec01ccf0c6d132c7fb
Conor Meade <cmeade6479@gmail.com>
2024-08-05 21:08:30 -0400

    Update supabaseClient.php

--- public/supabaseClient.php
+++ public/supabaseClient.php
@@ -2,8 +2,8 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
-// require(__DIR__.'/../vendor/autoload.php');
-require '/vendor/autoload.php';
+require(__DIR__.'/../vendor/autoload.php');
+// require '/vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
