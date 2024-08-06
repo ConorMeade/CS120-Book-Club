83e13ec1010ccd30cf3ea060fde2ebefc9baa2d1
Conor Meade <cmeade6479@gmail.com>
2024-08-06 11:27:17 -0400

    move stuff around

--- public/fetchBooks.js
+++ public/fetchBooks.js
@@ -1,11 +1,13 @@
-async function fetchBooks() {
+async function getUserBooks() {
 
+    console.log('fetching user books')
     // const supabaseUrl = 'https://ojygetcgjabzpxbmdaax.supabase.co';
     // const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qeWdldGNnamFienB4Ym1kYWF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzNDUyNzEsImV4cCI6MjAzNjkyMTI3MX0.K7M16UbjRvJ7YLf7YcTgEdmCbXz5rq1_jV8ERNUqxFA';
     // const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-    const url = 'https://infinite-beyond-05850-58f77000e905.herokuapp.com/fetchBooks.php'
+
     try {
+        const url = 'https://infinite-beyond-05850-58f77000e905.herokuapp.com/fetchBooks.php'
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
