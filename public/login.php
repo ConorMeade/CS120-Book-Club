e2c2b1768fa74ecd26049cce24a2bc25f7335bff
Brandon <brandonlwilson19@gmail.com>
2024-08-05 22:51:48 -0400

    redirect

--- public/login.php
+++ public/login.php
@@ -19,7 +19,7 @@ $password = $data['password'];
 $user = supabase_fetch_user_by_email($email);
 
 if ($user && password_verify($password, $user['password'])) {
-    echo json_encode(["status" => "success"]);
+    // echo json_encode(["status" => "success"]);
     echo json_encode(["status" => "success", "redirect_url" => "main_page.html"]);
 } else {
     echo json_encode(["status" => "error", "message" => "Invalid credentials"]);
