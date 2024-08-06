02cbcba54b7e411e4e2637d7bd927322c34e0ee5
Conor Meade <cmeade6479@gmail.com>
2024-08-05 22:53:56 -0400

    more clean up

--- public/supabaseClient.php
+++ public/supabaseClient.php
@@ -3,8 +3,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 ini_set('display_errors', 1);
-// require(__DIR__.'/../vendor/autoload.php');
-require '../vendor/autoload.php';
+require(__DIR__.'/../vendor/autoload.php');
+// require '../vendor/autoload.php';
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
